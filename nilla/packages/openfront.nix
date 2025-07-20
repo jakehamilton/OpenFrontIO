0c3188037f33962743eb100ae7554b86e8e87942
Jake Hamilton <jake.hamilton@hey.com>
2025-07-19 20:22:25 -0700

    feat: add nix support

---
+++ nilla/packages/openfront.nix
@@ -0,0 +1,48 @@
+{
+  config.packages.openfront = {
+    systems = [ "x86_64-linux" ];
+
+    package = { lib, buildNpmPackage, importNpmLock, nodejs, pkg-config, pixman, cairo, pango, makeWrapper, ... }:
+      let
+        src = ../..;
+      in
+      buildNpmPackage {
+        pname = "openfront";
+        version = "unstable";
+
+        inherit src;
+        inherit nodejs;
+
+        GIT_COMMIT = "unstable";
+
+        npmDeps = importNpmLock {
+          npmRoot = src;
+        };
+
+        npmConfigHook = importNpmLock.npmConfigHook;
+
+        nativeBuildInputs = [
+          pkg-config
+          makeWrapper
+        ];
+
+        buildInputs = [
+          pixman
+          cairo
+          pango
+        ];
+
+        installPhase = ''
+          mkdir -p $out/bin
+          mkdir -p $out/libexec/openfront
+
+          cp -r . $out/libexec/openfront
+
+          makeWrapper ${lib.getExe nodejs} $out/bin/openfront \
+            --add-flags "--loader ts-node/esm" \
+            --add-flags "--experimental-specifier-resolution=node" \
+            --add-flags "$out/libexec/openfront/src/server/Server.ts"
+        '';
+      };
+  };
+}
